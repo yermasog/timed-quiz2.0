467af15bee29cf331fee2927433944bf05ebda7e
ymasog <yer.masog@gmail.com>
2021-01-21 21:17:52 -0600

    created function layout

---
+++ assets/JS/script.js
@@ -0,0 +1,19 @@
+var startButton = document.getElementById("start-btn")
+
+startButton.addEventListener("click", startGame)
+
+
+
+function startGame() {
+console.log("started")
+startButton.classList.add("hide")
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
+
