20d32f717006694ff86b106e855e8987f0fde68d
ymasog <yer.masog@gmail.com>
2021-01-21 21:22:50 -0600

    created startGame function

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,12 +1,14 @@
-var startButton = document.getElementById("start-btn")
+var startButton = document.getElementById("start-btn");
+var questionContainerElements = document.getElementById("questions-container");
 
-startButton.addEventListener("click", startGame)
+startButton.addEventListener("click", startGame);
 
 
 
 function startGame() {
-console.log("started")
-startButton.classList.add("hide")
+console.log("started");
+startButton.classList.add("hide");
+questionContainerElements.classList.remove("hide");
 }
 
 function setNextQuestion() {
