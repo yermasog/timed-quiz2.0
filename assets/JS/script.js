a2e5b772da95e20fa64fdb50ddff854e13a043fd
ymasog <yer.masog@gmail.com>
2021-01-21 22:12:34 -0600

    created a function to tell whether or not the answer is correct and change the screen color

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -48,7 +48,17 @@ function selectAnswer() {
     var selectedButton = e.target
     var correct = selectedButton.dataset.correct;
     setStatusClass(document.body, correct);
-    Array.from(answerButtonsElement.children).forEach(button=> {setStatusClass(button, button.dataset.correct)})
+    Array.from(answerButtonsElement.children).forEach(button => {setStatusClass(button, button.dataset.correct)
+    })
+}
+
+function SetStatusClass (element, correct) {
+    clearStatusClass(element);
+    if (correct) {
+        element.classList.add("correct");     
+    } else {
+        element.classList.add("wrong")
+    }
 }
 
 var questions = [
