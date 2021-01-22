3adfae5d55cbf114639fa60cef22f9aa5f19dcb4
ymasog <yer.masog@gmail.com>
2021-01-21 21:47:48 -0600

    added more questions to question array

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -34,7 +34,38 @@ var questions = [
     {
         question: "Who killed Ramsey Bolton?",
         answers: [
-            {te}
+            {text: "Here is a hint", correct: false},
+            {text:"It wasn't Jon Snow", correct: false},
+            {text: "So the answer should be obvious", correct: false},
+            {text: "Sansa Stark", correct: true}
         ]
+    },
+    {
+        question: "Who killed the Night King?",
+        answers: [
+            {text: "Jorah Mormont", correct: false},
+            {text: "Arya Stark", correct: true},
+            {text: "Theon Greyjoy", correct: false},
+            {text: "Jon Snow", correct: false}
+        ]
+    },
+    {
+        question: "Who was NOT killed by Arya Stark?",
+        answers: [
+            {text: "the Night King", correct: false},
+            {text: "Walder Frey", correct: false},
+            {text: "Cersei Lannister", correct: true},
+            {text: "Petyr Baelish", correct: false}
+        ]
+    },
+    {
+        question: "Who killed Daenerys Targaryen?",
+        answers: [
+            {text: "Jon Snow", correct: false},
+            {text: "bad writing", correct: false},
+            {text: "David Benioff and DB Weiss", correct: false},
+            {text: "ALL OF THE ABOVE", correct: true}
+        ]
+
     }
 ]
