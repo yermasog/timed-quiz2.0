2ba39179e9a2445ef9a46d0217e4a917c0e4aac8
ymasog <yer.masog@gmail.com>
2021-01-21 22:01:26 -0600

    created showQuestion function

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -24,7 +24,11 @@ function showQuestion(question ) {
     question.answers.forEach(answer => {
         var button = document.createElement("button");
         button.innerText = answer.text;
-        button.classList.add()
+        button.classList.add("btn");
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
     })
      
 }
